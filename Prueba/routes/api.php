f22d08c926027f6bfbae7e7910e4b5357274a989
Alejandro2212 <gonzalezsandovalomaralejandro@gmail.com>
2023-09-15 21:58:41 -0600

    Comente lo que no se utiliza

--- Prueba/routes/api.php
+++ Prueba/routes/api.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\api\ApiController;
 use App\Http\Controllers\api\v2\ApiController as ApiV2Controller;
 use App\Http\Controllers\Monitores_Controller;
 
+/* 
+
+Ejemplos vistos en clase
+
 Route::prefix('v1')->group(function () {
     Route::get('/welcome/{numero}/{numero2?}', [ApiController::class, 'index'])
         ->where(['numero', '[0-9]+', 'numero2', '[0-9]+'])
@@ -18,7 +22,7 @@ Route::prefix('v2')->group(function () {
     Route::put('/personas/edit/{id}', [ApiV2Controller::class, 'update'])
         ->where(['id', '[0-9]+']);
 });
-
+*/
 Route::prefix('v1')->group(function () {
     Route::post('/Monitores', [Monitores_Controller::class, 'store']);
     Route::get('/Monitores', [Monitores_Controller::class, 'index']);
